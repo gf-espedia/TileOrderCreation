6120ad37e2cf2a9233ec964981ff4216e4be41ef
gf-espedia <giulia.favaro@espedia.it>
2019-07-18 13:50:03 +0000

    Update in order to work in any portal

--- webapp/controller/TileOrderCreationView1.controller.js
+++ webapp/controller/TileOrderCreationView1.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("TileOrderCreation.TileOrderCreation.controller.TileOrderCreationView1", {
 		onInit: function () {
-			window.location.replace("https://flpnwc-m9a44f3468.dispatcher.hana.ondemand.com/sites/espediasimplemaintenance#Shell-home ");
+			history.back();
 			window.open("https://order-m9a44f3468.dispatcher.hana.ondemand.com", '_blank');
 		}
 	});
